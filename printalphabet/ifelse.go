0c5d02f4b5e838fcb3e28fea1d64496e91ec078f
Uakbayev Meirkhat <uakbayevmeirkhat@MacBook-Air-Uakbayev.local>
2025-03-08 01:07:31 +0500

    yes

---
+++ printalphabet/ifelse.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 40
+	if a >= 140 {
+		fmt.Println("asdasd")
+	} else if a >= 200 {
+		fmt.Println("asda")
+	} else {
+		fmt.Println("123")
+	}
+}
